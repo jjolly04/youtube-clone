c520b6a6f700fc0de3773410f741ca7e28e68cc3
jjolly04 <judejolly2004@gmail.com>
2024-08-02 18:00:18 -0500

    Add process to convert file to 360p using ffmpeg

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,37 @@
 import express, { response } from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.post("/process-video", (req, res) => {
+  //Get path of input video file from request body
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  if (!inputFilePath || !outputFilePath) {
+    res
+      .status(400)
+      .send(
+        `Bad request. Missing file path:  ${
+          !outputFilePath ? "outputFilePath" : "inputFilePath"
+        }`
+      );
+  }
+
+  ffmpeg(inputFilePath)
+    .outputOptions("-vf", "scale=640:360") //360p
+    .on("end", () => {
+      res.status(200).send("Processing finished successfully.");
+    })
+    .on("error", (err) => {
+      console.log(`An error occurred: ${err.message}`);
+      res.status(500).send(`Internal Server Error: ${err.message}`);
+    })
+    .save(outputFilePath);
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Video Processing Service listening at http://localhost:${port}`);
 });
